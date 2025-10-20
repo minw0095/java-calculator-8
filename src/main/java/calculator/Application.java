b94109002f884799d65c2064da786ed631d48023
minw0095 <kmu6898@naver.com>
2025-10-20 18:44:48 +0900

    feat(Application): 실행을 위한 메서드 연결

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.Calendar;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+//        String n = "//;\n1;2;3";
+        String n = InputSystem.getInstance().input();
+        Separator separator = new Separator(n);
+//        System.out.println(Arrays.toString(separator.findNumbers()));
+
+        Numbers numbers = new Numbers(n, separator);
+        Calculator calculator = new Calculator(numbers.setNumbers());
+//        System.out.println(Arrays.toString(numbers.setNumbers()));
+
+        System.out.println(calculator.plus());
+
+
+
+
+
     }
 }
